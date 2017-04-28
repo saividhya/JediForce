af9aba255f7c0eaf178bd70391ef6691045618f8
PGunaraj <pgunaraj@asu.edu>
2017-04-27 22:56:23 -0700

    Create h.c

---
+++ source auditing/h.c
@@ -0,0 +1,12 @@
+#include <string.h>
+ /* strcat */
+ char *(strcat)(char *restrict s1, const char *restrict s2)
+ {
+     char *s = s1;
+     /* Move s so that it points to the end of s1.  */
+     while (*s != '\0')
+         s++;
+     /* Copy the contents of s2 into the space at the end of s1.  */
+     strcpy(s, s2);
+     return s1;
+ }
