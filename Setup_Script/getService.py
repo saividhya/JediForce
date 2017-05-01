286f9398099a55fa2a4a3e398151499cce08b950
saividhya <saividhya.91@gmail.com>
2017-04-30 22:29:58 -0700

    GetService and Targets

--- Setup_Script/getService.py
+++ Setup_Script/getService.py
@@ -9,9 +9,23 @@ i = iCTF("http://%s/" % ip)
 #Update Password
 t = i.login(team,"3VXEHUbdM4FG")
 serviceList = t.get_service_list()
-print serviceList
+print "Service List:"
+print "------------------------------------------------------------------------------------------------------------------------"
+print "Service ID\t\tPort\t\tService Name\t\tService Description\t\t"
+print "------------------------------------------------------------------------------------------------------------------------"
 for i in range(0,len(serviceList)):
-	service = serviceList[i]['service_id']				
+	print str(serviceList[i]['service_id'])+ "\t\t\t"+str(serviceList[i]['port']) + "\t\t" + serviceList[i]['service_name'] + "\t\t" + serviceList[i]['description']
+print "------------------------------------------------------------------------------------------------------------------------"
+print "\n\nTargets:"
+print "------------------------------------------------------------------------------------------------------------------------"					
+for i in range(0,len(serviceList)):
+	service = serviceList[i]['service_id']	
+	print "\n"
+	print str(service) + ":"		
 	hosts = t.get_targets(service)								
-	print hosts
-						
+	print "------------------------------------------------------------------------------------------------------------------------"
+	print "Team Name\t\tTeam Host\t\tFlag ID\t\tport\t\t"
+	print "------------------------------------------------------------------------------------------------------------------------"
+	for j in range(0,len(hosts['targets'])):
+		print str(hosts['targets'][j]['team_name'])+ "\t\t\t"+str(hosts['targets'][j]['hostname']) + "\t\t" + str(hosts['targets'][j]['flag_id']) + "\t\t" + str(hosts['targets'][j]['port'])
+	print "------------------------------------------------------------------------------------------------------------------------"					
